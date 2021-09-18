6e151191b5297c9d174db18a8fb4220c356dadfe
BRICE KENGNE <brice.kengne@gmail.com>
2021-09-18 23:21:56 +0000

    [ADD] View Module

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -22,7 +22,10 @@
     'depends': ['base'],
     
     'data': [
-        
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',    
         
     ],
     
